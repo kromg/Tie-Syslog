58fe71cde7d65160d02785716ac72fe4ecfa6be8
Giacomo Montagner <kromg@entirelyunlike.net>
2012-12-27 11:49:46 +0100

    v0.00_06 - accessors/mutators; eval { syslog() };
    - added accessors/mutators;
    - changed PRINT/PRINTF to eval {syslog()} so that exceptions can
      be caught and croak()ed the proper way
    - moved POD to Syslog.pod

--- lib/Tie/Syslog.pm
+++ lib/Tie/Syslog.pm
@@ -1,6 +1,6 @@
 package Tie::Syslog;
 
-our $VERSION = '0.00_05';
+our $VERSION = '0.00_06';
 
 use 5.006;
 use strict;
@@ -33,7 +33,7 @@ my @mandatory_opts = (@openlog_opts, 'priority');
 my %open_handles;
 
 # ------------------------------------------------------------------------------
-# 'Private' functions and methods
+# 'Private' functions
 # ------------------------------------------------------------------------------
 sub _connected() {
     return $Sys::Syslog::connected;
@@ -43,6 +43,19 @@ sub _check_params {
     # Wrong number of parameters in initialization
     croak "Odd number of elements in Tie","::","Syslog hash options"
         if @_ % 2;
+}
+
+# ------------------------------------------------------------------------------
+# Accessors/mutators
+# ------------------------------------------------------------------------------
+
+for my $opt (@mandatory_opts) {
+    no strict 'refs';
+    *$opt = sub {
+        my $self = shift;
+        $self->{$opt} = shift if @_;
+        return $self->{$opt};
+    };
 }
 
 # ------------------------------------------------------------------------------
@@ -108,14 +121,18 @@ sub CLOSE {
 sub PRINT {
     my $self = shift;
     carp "Cannot PRINT to a closed filehandle!" unless $self->{'is_open'};
-    syslog $self->{'priority'}, @_;
+    eval { syslog $self->facility."|".$self->priority, @_ };
+    croak "PRINT failed with errors: $@"
+        if $@;
 }
 
 sub PRINTF {
     my $self = shift;
     carp "Cannot PRINTF to a closed filehandle!" unless $self->{'is_open'};
     my $format = shift;
-    syslog $self->{'priority'}, $format, @_;
+    eval { syslog $self->facility."|".$self->priority, $format, @_ };
+    croak "PRINTF failed with errors: $@"
+        if $@;
 }
 
 
@@ -171,99 +188,5 @@ sub AUTOLOAD {
     $self->$name;
 }
 
-1; # End of Tie::Syslog
+'End of Tie::Syslog'
 __END__
-
-=head1 NAME
-
-Tie::Syslog - The great new Tie::Syslog!
-
-=head1 VERSION
-
-Version 0.00_05
-
-
-=head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Tie::Syslog;
-
-    my $foo = Tie::Syslog->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-
-=head2 function2
-
-
-=head1 AUTHOR
-
-Giacomo Montagner, C<< <kromg at entirelyunlike.net> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-tie-syslog at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Tie-Syslog>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Tie::Syslog
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Tie-Syslog>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Tie-Syslog>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Tie-Syslog>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Tie-Syslog/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2012 Giacomo Montagner.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
-
-=cut
-
-
